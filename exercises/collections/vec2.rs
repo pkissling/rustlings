19bb84f9d2ca50310bb810075bbd6f257e13b42b
Patrick Kissling <patrick.kissling@gmx.de>
2020-12-25 16:31:58 +0100

    Finish collections

--- exercises/collections/vec2.rs
+++ exercises/collections/vec2.rs
@@ -7,16 +7,14 @@
 // Execute the command `rustlings hint collections2` if you need
 // hints.
 
-// I AM NOT DONE
-
 fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
-    for i in v.iter_mut() {
-        // TODO: Fill this up so that each element in the Vec `v` is
-        // multiplied by 2.
+    let mut ret = Vec::new();
+    for i in v.iter() {
+        ret.push(*i + *i);
     }
 
     // At this point, `v` should be equal to [4, 8, 12, 16, 20].
-    v
+    ret
 }
 
 #[cfg(test)]
